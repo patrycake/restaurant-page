a1720daa3aaac63362e8ca7bb9da13a2153c8624
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-12-14 22:41:16 -0500

    simplify index.js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,5 @@
-import './style.css';
-import {
-    homeComp,
-    contactComp,
-    menuComp
-} from './components';
-
-/* BASIC!! */
 (() => {
     console.log("index.js console!");
-    const navBarDiv = document.createElement('div');
-    const navBar = document.createElement('nav');
-    const navBarList = document.createElement('ul');
-    const navBarTabHome = document.createElement('li');
-    const navBarTabContact = document.createElement('li');
-    const navBarTabMenu = document.createElement('li');
-
-    navBarTabHome.innerText = "Home";
-    navBarTabContact.innerText = "Contact";
-    navBarTabMenu.innerText = "Menu";
-
-    navBarTabHome.addEventListener('click', function () {
-        tabClickListener(homeComp)
-    })
-    navBarTabContact.addEventListener('click', function () {
-        tabClickListener(contactComp)
-    })
-    navBarTabMenu.addEventListener('click', function () {
-        tabClickListener(menuComp)
-    })
-
-    navBarList.classList.add('narbar-ol');
-    navBarDiv.classList.add('navbar-container');
-
-    navBarList.appendChild(navBarTabHome)
-    navBarList.appendChild(navBarTabContact)
-    navBarList.appendChild(navBarTabMenu)
-
-    navBar.appendChild(navBarList)
-    navBarDiv.appendChild(navBar)
-
-    console.log("navbar added");
-    console.log(document.getElementsByName('header'))
-    document.getElementById('header').appendChild(navBarDiv);
-    document.getElementById('content').appendChild(homeComp())
+    document.body.innerText = "Main";
 
 })();
-
-function tabClickListener(comp) {
-    document.getElementById("content").innerHTML = "";
-    document.getElementById('content').appendChild(comp())
-
-}
